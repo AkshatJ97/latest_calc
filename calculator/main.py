d5188ee0059b50c6ff5bc82308a114d453bcfa87
AkshatJ1997 <aj682@njit.edu>
2021-12-01 00:45:52 -0500

    part 1 files

---
+++ calculator/main.py
@@ -0,0 +1,21 @@
+class Calculator:
+    def __init__(self):
+        pass
+
+    result=0
+
+    def addition(self,value_a,value_b):
+        self.result=value_a+value_b
+        return self.result
+
+    def subtraction(self,value_a,value_b):
+        self.result=value_a-value_b
+        return self.result
+
+    def multiplication(self,value_a,value_b):
+        self.result=value_a * value_b
+        return self.result
+
+    def division(self,value_a,value_b):
+        self.result=value_a / value_b
+        return self.result
