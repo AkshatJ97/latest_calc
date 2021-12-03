6658ac91726bd02afe70a1b16d9e3374d51acb67
AkshatJ1997 <aj682@njit.edu>
2021-12-03 02:34:07 -0500

    calculator_test to test the calculator

---
+++ tests/calculator_test.py
@@ -0,0 +1,27 @@
+
+from calculator.main import Calculator
+
+
+def test_calculator_result():
+    calc = Calculator()
+    assert calc.get_result() == 0
+
+def test_calculator_add():
+    calc = Calculator()
+    calc.addition(10, 10)
+    assert calc.get_result() == 20
+
+def test_calculator_subtract():
+    calc = Calculator()
+    calc.subtraction(10, 10)
+    assert calc.get_result() == 0
+
+def test_calculator_multiply():
+    calc = Calculator()
+    calc.multiplication(10, 10)
+    assert calc.get_result() == 100
+
+def test_calculator_divide():
+    calc = Calculator()
+    calc.division(10, 10)
+    assert calc.get_result() == 1
