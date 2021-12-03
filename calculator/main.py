6a2762f23837c45ce4f3fc84b84c53011e0f069d
AkshatJ1997 <aj682@njit.edu>
2021-12-03 03:33:30 -0500

    we edited the code because we added calculator with history

--- calculator/main.py
+++ calculator/main.py
@@ -1,24 +1,54 @@
+""" importing all the methods from calc_methods"""
+from calc_methods.addition import Addition
+from calc_methods.subtraction import Subtraction
+from calc_methods.multiplication import Multiplication
+from calc_methods.division import Division
+
+
 class Calculator:
-    def __init__(self):
-        pass
+    # result set to 0 for initialization
+    history = []
 
-    result=0
+    @staticmethod
+    def clear_history():
+        Calculator.history.clear()
 
-    def addition(self,value_a,value_b):
-        self.result=value_a+value_b
-        return self.result
+    @staticmethod
+    def add_calculation_to_history(calculation):
+        Calculator.history.append(calculation)
 
-    def subtraction(self,value_a,value_b):
-        self.result=value_a-value_b
-        return self.result
+    @staticmethod
+    def get_first_calculation_history():
+        return Calculator.history[0]
 
-    def multiplication(self,value_a,value_b):
-        self.result=value_a * value_b
-        return self.result
+    @staticmethod
+    def get_last_calculation_added():
+        return Calculator.history[-1]
 
-    def division(self,value_a,value_b):
-        try:
-            self.result=value_a / value_b
-            return self.result
-        except ZeroDivisionError:
-            return 0
+    @staticmethod
+    def get_calculation_count():
+        return len(Calculator.history)
+
+    @staticmethod
+    def add_nums(value_a, value_b):
+        addition = Addition.create(value_a, value_b).getresult()
+        Calculator.add_calculation_to_history(addition)
+        return Calculator.get_last_calculation_added()
+
+    @staticmethod
+    def subtract_nums(value_a, value_b):
+        subtraction = Subtraction.create(value_a, value_b).getresult()
+        Calculator.add_calculation_to_history(subtraction)
+        return Calculator.get_last_calculation_added()
+
+    @staticmethod
+    def multiply_nums(value_a, value_b):
+        multiplication = Multiplication.create(value_a, value_b).getresult()
+        Calculator.add_calculation_to_history(multiplication)
+        return Calculator.get_last_calculation_added()
+
+    @staticmethod
+    def divide_nums(value_a, value_b):
+        division = Division.create(value_a, value_b).getresult()
+        Calculator.add_calculation_to_history(division)
+        return Calculator.get_last_calculation_added()
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,27 +1,39 @@
-
+import pprint
+import pytest
 from calculator.main import Calculator
 
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
 
-def test_calculator_result():
-    calc = Calculator()
-    assert calc.get_result() == 0
 
-def test_calculator_add():
-    calc = Calculator()
-    calc.addition(10, 10)
-    assert calc.get_result() == 20
+def test_calculator_add(clear_history):
+    """ To check if calculator addition result is correct """
+    assert Calculator.add_nums(10, 10) == 20
+    assert Calculator.add_nums(20, 20) == 40
+    assert Calculator.add_nums(30, 30) == 60
+    assert Calculator.add_nums(40, 40) == 40
+    assert Calculator.get_calculation_count() == 4
+    assert Calculator.get_first_calculation_history() == 20
+    pprint.pprint(Calculator.history)
 
-def test_calculator_subtract():
-    calc = Calculator()
-    calc.subtraction(10, 10)
-    assert calc.get_result() == 0
 
-def test_calculator_multiply():
-    calc = Calculator()
-    calc.multiplication(10, 10)
-    assert calc.get_result() == 100
+def test_calculator_subtract(clear_history):
+    """ To check if calculator subtraction result is correct """
+    assert Calculator.subtract_nums(10, 20) == -10
+    assert Calculator.get_calculation_count() == 1
+    pprint.pprint(Calculator.history)
 
-def test_calculator_divide():
-    calc = Calculator()
-    calc.division(10, 0)
-    assert calc.get_result() == 0
+
+def test_calculator_multiply(clear_history):
+    """ To check if calculator multiplication result is correct """
+    assert Calculator.multiply_nums(10, 10) == 100
+    assert Calculator.get_calculation_count() == 1
+    pprint.pprint(Calculator.history)
+
+
+def test_calculator_divide(clear_history):
+    """ To check if calculator division result is correct """
+    assert Calculator.divide_nums(20, 20) == 1
+    assert Calculator.get_calculation_count() == 1
+    pprint.pprint(Calculator.history)
